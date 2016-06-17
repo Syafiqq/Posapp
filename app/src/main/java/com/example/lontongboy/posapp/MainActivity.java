cc39a8ebcc53345770ad818a6e4ca003c48166bf
dcopz <dcopzkey@gmail.com>
2016-06-17 08:43:52 +0700

    yity

--- app/src/main/java/com/example/lontongboy/posapp/MainActivity.java
+++ app/src/main/java/com/example/lontongboy/posapp/MainActivity.java
@@ -1,13 +1,54 @@
 package com.example.lontongboy.posapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button scanBtn;
+    private TextView formatTxt, contentTxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        wewerer
+        setContentView(R.layout.main_layout);
+
+        scanBtn = (Button) findViewById(R.id.scan_button);
+        formatTxt = (TextView) findViewById(R.id.scan_format);
+        contentTxt = (TextView) findViewById(R.id.scan_content);
+
+        scanBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.scan_button){
+            IntentIntegrator scanIntegrator = new IntentIntegrator(this);
+            scanIntegrator.initiateScan();
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult scanningResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (scanningResult != null){
+            String scanContent = scanningResult.getContents();
+            String scanFormat = scanningResult.getFormatName();
+
+            formatTxt.setText("FORMAT : " + scanFormat);
+            contentTxt.setText("CONTENT : " + scanContent);
+        }
+        else{
+            Toast toast = Toast.makeText(getApplicationContext(), "No scan data", Toast.LENGTH_LONG);
+            toast.show();
+        }
     }
 }
