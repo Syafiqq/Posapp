31427a0469c6c1e4a735bd572ead9371f3727d68
z430 <maystya@gmail.com>
2016-10-02 19:44:13 +0800

    moved

--- app/src/main/java/com/example/lontongboy/posapp/CheckBarang.java
+++ app/src/main/java/com/example/lontongboy/posapp/CheckBarang.java
@@ -2,6 +2,7 @@ package com.example.lontongboy.posapp;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,22 +10,33 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 // TODO: 10/1/16 query barang di tampilkan d list view, udah gitu aja 
 
 public class CheckBarang extends AppCompatActivity {
 
-    private Button cekBarang;
     public String hasilScan;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_check_barang);
-        cekBarang = (Button) this.findViewById(R.id.btn_check);
+        Button cekBarang = (Button) this.findViewById(R.id.btn_check);
         final Activity activity = this;
         cekBarang.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,25 +51,81 @@ public class CheckBarang extends AppCompatActivity {
 
             }
         });
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
-        if(result != null){
-            if (result.getContents() == null){
+        DBInter inter = new DBInter();
+        inter.initDBConnection();
+
+        if (result != null) {
+            if (result.getContents() == null) {
                 Log.d("MainActivity", "Canceled scan");
                 Toast.makeText(this, "cancelled", Toast.LENGTH_LONG).show();
 
-            } else{
+            } else {
                 Log.d("MainActivity", "Scanned");
                 Toast.makeText(this, "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
                 hasilScan = result.getContents();
                 System.out.println(hasilScan);
 
+                // dicocokkan
+                String sql = "SELECT id_barcode FROM tb_barang";
+                try {
+                    ResultSet rs = inter.stmt.executeQuery(sql);
+                    while (rs.next()){
+                        String id  = rs.getString("id_barcode");
+                        if(id.equals(hasilScan))
+                            Log.d("PODO", "asdasd");
+                    }
+
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+
             }
-        } else{
+        } else {
             super.onActivityResult(requestCode, resultCode, data);
         }
+    }
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("CheckBarang Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
     }
 }
